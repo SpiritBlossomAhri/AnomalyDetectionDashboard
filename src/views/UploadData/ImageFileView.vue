cef5d4e794eb3042f44dea79853a651bbe3b96a2
SpiritBlossomAhri <dns05018@kakao.com>
2022-07-20 02:52:23 +0900

    오타 수정

--- src/views/UploadData/ImageFileView.vue
+++ src/views/UploadData/ImageFileView.vue
@@ -6,7 +6,7 @@ import { ref } from "vue";
     <div class="common-layout">
         <el-container>
             <el-main>
-                <h1>Time Series Data - Image File Upload.</h1>
+                <h1>Image File Upload.</h1>
                 <p>Test for uploading image file.</p>
                 <ImageFileUpload />
             </el-main>
